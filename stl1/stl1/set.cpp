16c83a6727548aab17a6399d1f2d1c0b7ee129c5
konalo-X <konalo_bill@sina.com>
2016-09-11 01:43:11 +0800

    set_union用法 copy（X,X,ostream） for_each(X,X,函数对象)

--- stl1/stl1/set.cpp
+++ stl1/stl1/set.cpp
@@ -11,6 +11,7 @@
 #include <iterator>
 #include <set>
 #include <list>
+#include <algorithm>
 #include <fstream>
 const int NUM=6;
 
@@ -21,17 +22,27 @@ int main()
     fin.open("/Users/konalo/MyC--/data.txt");
     string s1[6]={"buffoon","thinkers","for","heavey","can","for"};
     set<string>A(s1,s1+NUM);
-    list<string>B;
+    
+    set<string>B;
     ostream_iterator<string,char> out(cout," ");
 
     copy(A.begin(), A.end(), out);
     cout<<endl<<"input:";
-    
+   
     copy(istream_iterator<string,char>(fin), istream_iterator<string,char>(),
-         insert_iterator<list<string> >(B,B.begin()));
+         insert_iterator<set<string> >(B,B.begin()));
             cout<<"输出流：";
+   // copy(A.begin(), A.end(), insert_iterator<set<string>>(B,B.begin()));
     copy(B.begin(), B.end(), out);
     cout<<endl;
+    
+    cout<<"两个set集合求并集的算法"<<endl;
+    set<string>C;
+    //set_union(A.begin(), A.end(), B.begin(), B.end(), out);
+    set_union(A.begin(), A.end(), B.begin(), B.end(), insert_iterator<set<string>>(C,C.begin()));
+    copy(C.begin(), C.end(), out);
     fin.close();
+    
+ 
     return 0;
 }
